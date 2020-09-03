fb7c4ac15f8c6f3cb222e04d978010e3341a2536
Nguyen Nga <ngant1096@gmail.com>
2020-09-03 15:18:31 +0700

    update

---
+++ image-gallery/asset/js/script.js
@@ -0,0 +1,111 @@
+$(function() {
+    $(".item-1").click(function() {
+        $(".gallery, .heading").hide();
+        $(".sub-item").append(`
+            <h1>Tiện ích DA</h1>
+            <div class="gallery">
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích DA/1.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">Hồ nước mặn</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích DA/2.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">Xương cá</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích DA/3.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">Xương cá</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích DA/4.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">Tiện ích DA</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích DA/5.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">Tiện ích DA</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích DA/6.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">Bể bơi</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích DA/7.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">Bể bơi</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích DA/8.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">Gym</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích DA/9.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">Xương cá</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích DA/10.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">Sân chơi trẻ em</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích DA/11.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">Trường ĐH VinUni</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích DA/12.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">View ĐH VinUni</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích DA/13.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">Gym</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích DA/14.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">BBQ</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích DA/15.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">Sân chơi trẻ em</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích DA/16.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">Trường ĐH VinUni</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích DA/17.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">View ĐH VinUni</div>
+                </div>
+            </div>
+        `);
+    })
+
+    $(".item-2").click(function() {
+        $(".gallery, .heading").hide();
+        $(".sub-item").append(`
+            <h1>Tiện ích Ruby</h1>
+            <div class="gallery">
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích Ruby/1.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">Tiện ích 1</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích Ruby/2.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">Tiện ích 2</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích Ruby/3.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">Tiện ích 3</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích Ruby/4.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">Tiện ích 4</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích Ruby/5.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">Tiện ích 5</div>
+                </div>
+                <div class="gallery-item1">
+                    <img src="asset/images/Ảnh 3D tiện ích Ruby/6.jpg" alt="Ảnh 3D tiện ích DA" class="gallery-img">
+                    <div class="overlay">Tiện ích 6</div>
+                </div>
+            </div>
+        `);
+    })
+})
